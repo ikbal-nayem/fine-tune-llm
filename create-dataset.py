08ff53e8d089e07c30f46a4bf3907b72ca6551af
ikbal-nayem <ikbalnayem000@gmail.com>
2025-07-18 13:07:01 +0600

    generating dataset script is ready

--- create-dataset.py
+++ create-dataset.py
@@ -4,46 +4,39 @@ import json
 
 OLLAMA_MODEL = "gemma3:4b"
 
-response: ChatResponse = ollama.chat(model=OLLAMA_MODEL, messages=[
-    {
-        'role': 'user',
-        'content': 'What do you know about Bangladesh law? tell me in 2 sentences.',
-    },
-])
-
-print(response['message']['content'])
-
-
 def generatePrompt(law_item, language='en'):
     return f"""
     You are a legal QA generator for Bangladeshi law.
 
     Given the following law section, generate 3 unique QA pairs that a citizen might ask about this law. Each answer must include the section number and a clear, human-friendly explanation.
 
-    Provide questions and answers in {'Bangla' if language == 'bn' else 'English'}.
+    Provide questions and answers in 2 English and 1 Bangla language.
 
-    LAW NAME: {law_item['law_name_en']}
-    PART: {law_item['part_no_en']}
-    CHAPTER: {law_item['chapter_no_en']} - {law_item['chapter_name_en']}
-    SECTION NO: {law_item['section_no_en']}
-    SECTION NAME: {law_item['section_name_en']}
+    LAW NAME: {law_item.get('law_name_en')}
+    PART: {law_item.get('part_no_en')} - ({law_item.get('part_name_en')})
+    CHAPTER: {law_item.get('chapter_no_en')} - ({law_item.get('chapter_name_en')})
+    SECTION NO: {law_item.get('section_no_en')}
+    SECTION NAME: {law_item.get('section_name_en')}
     CONTENT:
-    {law_item['content']}
+    {law_item.get('content')}
 
+    **The output must be in JSON format even if there is only one QA pair**
     Format output as a JSON array of:
-    [
-        {{
-            "question": "...",
-            "answer": "...",
-            "law_reference": "Act name, Part, Chapter, Section number",
-            "input_context": "Full section content"
-        }},
-        ...
-    ]
+    {{
+        "output": [
+			{{
+				"question": "...",
+				"answer": "...",
+				"law_reference": "Act name, Part, Chapter, Section number", // If anything missing do not set to None or null just spik it.
+				"input_context": "Full content"
+			}},
+			...
+    	]
+    }}
     """
 
 
-def generateQA(law_item, language='en'):
+def generateQA(law_item, language='en')->list[object]:
     response: ChatResponse = ollama.chat(model=OLLAMA_MODEL, stream=False, messages=[
         {
             'role': 'user',
@@ -59,3 +52,26 @@ def generateQA(law_item, language='en'):
         print("Error parsing LLM response:", e)
         print("Response text:", response_text)
         return []
+
+
+def main():
+	with open('data/registration-act.json', 'r') as f:
+		law_items = json.load(f)
+        
+	dataset = []
+	for i, law_item in enumerate(law_items):
+		print(f"{i+1} - {law_item['section_name_en']}")
+		qa_pairs = generateQA(law_item, language='en')
+		print(json.dumps(qa_pairs, indent=2))
+		for qa in qa_pairs.get('output'):
+			dataset.append(qa)
+
+	with open('data/ft_dataset-registration-act.jsonl', 'w') as f:
+		for item in dataset:
+			f.write(json.dumps(item, ensure_ascii=False) + '\n')
+
+
+if __name__ == "__main__":
+    main()
+
+
