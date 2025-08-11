a42a477c2fc576a6af008602cfc27bbb3b8e4e5a
ikbal-nayem <ikbalnayem000@gmail.com>
2025-08-11 11:01:28 +0600

    update prompt

--- create-dataset.py
+++ create-dataset.py
@@ -28,7 +28,7 @@ def generatePrompt(law_items: list[dict]) -> str:
     return f"""
     You are a legal QA generator for Bangladeshi law.
 
-    Given the following law section, generate atleast 3 unique QA pairs that a citizen might ask about this law. The number of pairs will vary depending on content size (larger the content more question pairs) upto 50. Each answer must include the section number and a clear, human-friendly explanation.
+    Given the following law section, generate 3 to 50 unique QA pairs that a citizen might ask about this law. The number of pairs will vary depending on content size (larger the content more question pairs) upto 50. Each answer must include the section number and a clear, human-friendly explanation.
 
     Provide questions and answers will be in English and Bangla language, The ratio will be BN:EN=1:3.
 
@@ -44,8 +44,8 @@ def generatePrompt(law_items: list[dict]) -> str:
 			{{
 				"question": "...",
 				"answer": "...",
-				"law_reference": "Act name, Part, Chapter, Section numbers", // If anything missing do not set to None or null just spik it. If the context has is multiple content then set multiple section, chapter and part no if exists.
-                "context": "Summary of content with reference url" // If reference url exists on the context set it. If not just skip it.
+				"law_reference": <Act name, Part, Chapter, Section numbers>, // If anything missing do not set to None or null just spik it. If the context has is multiple content then set multiple section, chapter and part no if exists.
+                "context": <Summary of the content with reference url> // If reference url exists on the context set it. If not just skip it.
 			}},
 			...
     	]
@@ -78,7 +78,7 @@ def saveItems(file, items):
                 f.write("\n")
             print(f" {len(items.get('output'))} pairs")
     except Exception as e:
-        print("Error while parsing LLM output: ", e)
+        print("Error while parsing LLM output: ", e, items)
 
 def main():
     start_time = time.time()
@@ -92,29 +92,15 @@ def main():
         with open(file, 'r') as f:
             law_items = json.load(f)
         
-        # try:
-        #     with open(output_files[f_i], 'r') as f:
-        #         existing_data = json.load(f)
-        #         print(f"{len(existing_data)} data found")
-        # except FileNotFoundError:
-        #     existing_data = []
-
-        # dataset = []
         for i, law_item in enumerate(law_items):
             print(f"IDX {i+1} - Section [{law_item.get('section_no_en') or law_item.get('section_no_bn')}] ...", end="")
             qa_pairs = generateQA([law_item])
             saveItems(output_files[f_i], qa_pairs)
 
             rand_items = random.sample(law_items, i+1)
-            print(f"Random Sections {[item.get('section_no_en') or item.get('section_no_bn') for item in rand_items]} ...", end="")
+            print(f"\tRandom Sections {[item.get('section_no_en') or item.get('section_no_bn') for item in rand_items]} ...", end="")
             qa_pairs = generateQA(rand_items)
             saveItems(output_files[f_i], qa_pairs)
-
-        # with open(output_files[f_i], 'w') as f:
-        #     print(
-        #         f"Saving {len(dataset)} pairs to {output_files[f_i]}...", end="")
-        #     f.write(json.dumps(dataset, ensure_ascii=False) + '\n')
-        #     print("Done")
 
     execution_time = calculateDuration(start_time, time.time())
     print(
